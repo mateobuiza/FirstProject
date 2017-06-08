404803783b9128d9ac93e0355c2641fdbe9b86a2
IsaamBarwani <ib14aaf@herts.ac.uk>
2017-06-08 19:22:00 +0100

    Archer Class Created

---
+++ src/Archer.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Isaam on 08/06/2017.
+ */
+public class Archer extends Card{
+
+    private String bow;
+
+    public Archer(String name, int defence, int attack, String bow){
+        super(name,defence,attack);
+        this.bow = bow;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // lol kden
+        /*// lol kden
      
         Scanner scan = new Scanner(System.in);      // Reading input from System.in, which is the keyboard
         String s = scan.next();                     // reads in text
@@ -16,30 +16,24 @@ public class Main {
         System.out.println("Enter a number: ");     // prints out asking the user to enter a number
         int i = scan.nextInt();                     // reads in a nmber the user types in (must be an int or will stop program)
         System.out.println("The number you put in is: " + i);   // prints out what number the user typed in
-
+        */
 
         ArrayList<Card> deck = new ArrayList<>();
 
-        Card archer = new Card ("Archer", 10, 30);
-
+        Archer archer = new Archer ("Archer", 10, 30, "Hyrule Bow");
         Card warrior = new Card ("Warrior", 20, 20);
-
         Card kleoTheledge = new Card ("Kleo", 100, 100);
 
         deck.add (archer);
         deck.add (warrior);
         deck.add (kleoTheledge);
 
-
         int defence = 5;
         int attack = 5;
 
         for(int index = 0; index <= 16; index++)
-
         {
-
             Card npc = new Card ("Fred", defence, attack);
-
 
             defence = defence + 5;
             attack = attack + 5;
@@ -49,16 +43,11 @@ public class Main {
 
 
         Card first = deck.get(0);
-
-        System.out.println("The name of the card is " + first.getName());
-
-        System.out.println("First card attack " + first.getAttack());
-
-        System.out.println("First card defence " + first.getDefence());
-
-
         Card seventh = deck.get(6);
 
+        System.out.println("The name of the card is " + first.getName());
+        System.out.println("First card attack " + first.getAttack());
+        System.out.println("First card defence " + first.getDefence());
         System.out.println("The name of the card is " + seventh.getName() + " Attack is " + seventh.getAttack() + " Defence is " + seventh.getDefence());
 
         if (first.getAttack() > seventh.getDefence()) {
@@ -72,14 +61,6 @@ public class Main {
             System.out.println("Player 2 wins");
         }
 
-
-
-
-
-
-
-
     }
-
 
 }
