5e7014c63f05cce155f459ada4275052dd1a9291
KleoG <kleogeorgiou@hotmail.com>
2017-06-09 17:18:26 +0100

    Mateos task

---
+++ src/MateoTask.java
@@ -0,0 +1,23 @@
+/**
+ * Created by KleoG on 09/06/2017.
+ */
+
+/**
+ * Create two fields. One called name, which is a String, and one which is called age which is an integer.
+ *
+ * Create a constructor, without parameters. Set name to equal mateo, and age to equal 22.
+ *
+ * Create another constructor, which accepts 2 parameters, name and int. Using the parameters, initialise the fields you
+ * created.
+ *
+ * Create a method that accepts 3 integers in its parameters, works out the average of the three numbers and returns
+ * the answer.
+ *
+ * Create another method, which accepts 3 integers in its parameters and finds out which one is the smallest number.
+ * (possibly use if statements). Instead of returning the answer, print it out.
+ *
+ * Create a getter and setter for the 2 fields you created.
+ *
+ */
+public class MateoTask {
+}
--- src/Person.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Mateo on 08/06/2017.
- */
-public class People {
-}
